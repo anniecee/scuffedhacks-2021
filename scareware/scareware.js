0cbf8c477252caf6aee629b5681be8c126ea0500
Annie <cung.ngananh@gmail.com>
2021-05-01 16:46:59 -0700

    edit results scareware

--- scareware/scareware.js
+++ scareware/scareware.js
@@ -2,15 +2,14 @@ function displayAnswer() {
     console.log("working");
     if (document.getElementById('op-a').checked) {
         document.getElementById('result').style.color = 'red'
-        document.getElementById('result').innerHTML = 'Wrong!Hold on...take a deep breath. <br>Scareware will trick victims into feeling panicked and out of options.
-        The page often displays a warning with bright colours, bold text, and a company logo to gain your trust. Close the page and leave the site that gave it to you.''
+        document.getElementById('result').innerHTML = 'Wrong!'
+        document.getElementById('explanation').innerHTML = 'Hold on...take a deep breath. Scareware will trick victims into feeling panicked and out of options. The page often displays a warning with bright colours, bold text, and a company logo to gain your trust.Close the page and leave the site that gave it to you.'
     }
 
     if (document.getElementById('op-b').checked) {
         document.getElementById('result').style.color = 'limegreen'
-        document.getElementById('result').innerHTML = 'Correct! Scareware will trick victims into feeling panicked and out of options. 
-        The page often displays a warning with bright colours, bold text, and a company logo to gain your trust.Close the page and
-        leave the site that gave it to you.'
+        document.getElementById('result').innerHTML = 'Correct!'
+        document.getElementById('explanation').innerHTML = 'Scareware will trick victims into feeling panicked and out of options. The page often displays a warning with bright colours, bold text, and a company logo to gain your trust.Close the page and leave the site that gave it to you.'
     }
 
 }
