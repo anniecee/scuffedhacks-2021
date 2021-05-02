2dc302dd70ae4b7b338392b7944a3773b8206593
Dylan Feng <ddf3@sfu.ca>
2021-05-01 17:50:47 -0700

    changed title

--- hid/hid.js
+++ hid/hid.js
@@ -7,7 +7,7 @@ function displayAnswer()
 		document.getElementById('result').innerHTML = "Incorrect! "
         document.getElementById('explination').innerHTML = "Unknown USBs should stay away from your computer. " + 
 													   "Seemingly harmless USB drives might actually be dangerous hacking weapons. As soon as you plug it in, " + 
-													   "the fake USB is able to mimic a person typing on a keyboard. They are able to allow hackers to have" + 
+													   "the fake USB is able to mimic a person typing on a keyboard, which will allow hackers to have " + 
 													   "access to your computer without being in the same room."
 
     }
@@ -18,7 +18,7 @@ function displayAnswer()
 		document.getElementById('result').innerHTML = 'Incorrect! '
 		document.getElementById('explination').innerHTML = "Good thinking, but you could help others from plugging it in. " + 
 														   "Seemingly harmless USB drives might actually be dangerous hacking weapons. As soon as you plug it in, " +
-														   "the fake USB is able to mimic a person typing on a keyboard. They are able to allow hackers to have" +
+														   "the fake USB is able to mimic a person typing on a keyboard, which will allow hackers to have " + 
 														   "access to your computer without being in the same room."
 
 
@@ -29,7 +29,7 @@ function displayAnswer()
         document.getElementById('result').innerHTML = 'Correct! '
 		document.getElementById('explination').innerHTML = "Don't plug it in and keep it away from curious hands. " + 
 														   "Seemingly harmless USB drives might actually be dangerous hacking weapons. As soon as you plug it in, " + 
-														   "the fake USB is able to mimic a person typing on a keyboard. They are able to allow hackers to have " + 
+														   "the fake USB is able to mimic a person typing on a keyboard, which will allow hackers to have " + 
 														   "access to your computer without being in the same room."
 
     }
@@ -40,7 +40,7 @@ function displayAnswer()
         document.getElementById('result').innerHTML = 'Incorrect! '
 		document.getElementById('explination').innerHTML = "You might put someone's computer at risk. " + 
 														   "Seemingly harmless USB drives might actually be dangerous hacking weapons. As soon as you plug it in, " +
-														   "the fake USB is able to mimic a person typing on a keyboard. They are able to allow hackers to have " +
+														   "the fake USB is able to mimic a person typing on a keyboard, which will allow hackers to have " + 
 														   "access to your computer without being in the same room. " 
     }
 }
